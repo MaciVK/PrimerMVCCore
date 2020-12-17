ff38f6db3d89d1e2b5d34c31aef6e7bfd0455991
Gabri <gmacias96@gmail.com>
2020-12-17 08:15:55 +0100

    Add project files.

---
+++ PrimerMVCCore/Controllers/InformacionController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerMVCCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerMVCCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        public IActionResult VistaPost()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult VistaPost(string cajanombre, string cajaedad)
+        {
+            ViewBag.Nombre = cajanombre;
+            ViewBag.Edad = cajaedad;
+            return View();
+        }
+        public IActionResult VistaDatos(string aplicacion, int version)
+        {
+            ViewBag.Aplicacion = aplicacion;
+            ViewBag.Version = version;
+            return View();
+        }
+        public IActionResult Inicio()
+        {
+            ViewBag.Nombre = "Alumno";
+            ViewBag.Edad = 19;
+            Persona person = new Persona();
+            person.Nombre = "Modelo";
+            person.Edad = 111;
+            person.Genero = "Dunno";
+            return View(person);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PrimerMVCCore/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerMVCCore.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Genero { get; set; }
+
+    }
+}
